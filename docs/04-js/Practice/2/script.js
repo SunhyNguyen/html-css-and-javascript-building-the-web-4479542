c2bcb0e0481e53d5581402dab88650d3e26834ee
SunhyNguyen <anhnguyen273@usf.edu>
2025-03-17 14:36:16 +0000

    here we go

--- docs/04-js/Practice/2/script.js
+++ docs/04-js/Practice/2/script.js
@@ -8,27 +8,27 @@ function dragElement(terrariumElement) {
     console.log(e);
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onpointermove = elementDrag;
-    document.onpointerup = stopElementDrag;
+    document.onpointermove = ElementDrag;
+    document.onpointerup = stopElemntDrag;
   }
 
-  function elementDrag(e) {
+  function ElementDrag(e) {
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
 
-    terrariumElement.style.top = (terrariumElement.offsetTop - pos2) + 'px';
     terrariumElement.style.left = (terrariumElement.offsetLeft - pos1) + 'px';
+    terrariumElement.style.top = (terrariumElement.offsetTop - pos2) + 'px';
   }
 
-  function stopElementDrag() {
+  function stopElemntDrag() {
     document.onpointermove = null;
     document.onpointerup = null;
   }
 }
 
-dragElement(document.getElementById('plant1'));
+dragElement(document.getElementById('plant1'));  
 dragElement(document.getElementById('plant2'));
 dragElement(document.getElementById('plant3'));
 dragElement(document.getElementById('plant4'));
